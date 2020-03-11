dbaa70e1e58a948995ec4868f94727708b1a303d
Track <659956865@qq.com>
2020-03-11 10:04:46 +0800

    test:加载弹幕上

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,33 +20,11 @@ use Illuminate\Support\Facades\Redis;
 
 class UserController extends BaseController implements WebSocketHandlerInterface
 {
-    public $server;
 
     // 声明没有参数的构造函数
     public function __construct()
     {
-        $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9502);
-        $this->server->on('open', function (Swoole\WebSocket\Server $server, $request) {
-            echo "打开websocket成功";
-        });
-        $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
-            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-            $server->push($frame->fd, "this is server");
-        });
-        $this->server->on('close', function ($ser, $fd) {
-            echo "client {$fd} closed\n";
-        });
-        // $this->server->on('request', function ($request, $response) {
-        //     // 接收http请求从get获取message参数的值，给用户推送
-        //     // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
-        //     foreach ($this->server->connections as $fd) {
-        //         // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-        //         if ($this->server->isEstablished($fd)) {
-        //             $this->server->push($fd, $request->get['message']);
-        //         }
-        //     }
-        // });
-        $this->server->start();
+
     }
     // public function onOpen(Server $server, Request $request)
     // {
@@ -333,7 +311,29 @@ class UserController extends BaseController implements WebSocketHandlerInterface
 
         Redis::rpush($videoId,"$currentTime:$barrage");
 
-
+        $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9502);
+        $this->server->on('open', function (Swoole\WebSocket\Server $server, $request) {
+            echo "来自后端:打开websocket成功";
+            $ws->push($request->fd, "hello, welcome\n");
+        });
+        $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+            $server->push($frame->fd, "this is server");
+        });
+        $this->server->on('close', function ($ser, $fd) {
+            echo "client {$fd} closed\n";
+        });
+        // $this->server->on('request', function ($request, $response) {
+        //     // 接收http请求从get获取message参数的值，给用户推送
+        //     // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+        //     foreach ($this->server->connections as $fd) {
+        //         // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+        //         if ($this->server->isEstablished($fd)) {
+        //             $this->server->push($fd, $request->get['message']);
+        //         }
+        //     }
+        // });
+        $this->server->start();
 
         return Responder::success('成功发送弹幕');
     }
